6a36f8929e539bd60faa22ebfef968f6e8fb0f0b
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-08-06 19:02:57 +0200

    FS-42 fix: fix import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import '@/styles/index.scss';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { robotoReg, robotoMed, literalLight, literalBold, literalReg, ptSans, karton, karton2 } from '@/utils/fonts';
+import React from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
