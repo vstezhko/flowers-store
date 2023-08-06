eb83f3f1d5254df854c8d280c8bfe8a740e47af4
viktoriastezko <viktoriastezhko@gmail.com>
2023-08-06 21:44:31 +0200

    fix: fix prettier errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,35 +9,35 @@ import React from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-        <ThemeProvider theme={FSTheme}>
-    <html lang='en'>
-      <head>
-        <title>Flowers store</title>
-        <meta
-          name='description'
-          content="Discover the enchantment of nature\' s finest blooms. Fresh and artistic
+    <ThemeProvider theme={FSTheme}>
+      <html lang='en'>
+        <head>
+          <title>Flowers store</title>
+          <meta
+            name='description'
+            content="Discover the enchantment of nature\' s finest blooms. Fresh and artistic
                   floral arrangements for every occasion. Unforgettable moments await at our Flowers Store."
-        />
-        <style jsx global>{`
-          :root {
-            --literal-reg: ${literalReg.style.fontFamily};
-            --literal-bold: ${literalBold.style.fontFamily};
-            --literal-light: ${literalLight.style.fontFamily};
-            --roboto-reg: ${robotoReg.style.fontFamily};
-            --roboto-med: ${robotoMed.style.fontFamily};
-            --pt-sans: ${ptSans.style.fontFamily};
-            --karton: ${karton.style.fontFamily};
-            --karton2: ${karton2.style.fontFamily};
-          }
-        `}</style>
-      </head>
+          />
+          <style jsx global>{`
+            :root {
+              --literal-reg: ${literalReg.style.fontFamily};
+              --literal-bold: ${literalBold.style.fontFamily};
+              --literal-light: ${literalLight.style.fontFamily};
+              --roboto-reg: ${robotoReg.style.fontFamily};
+              --roboto-med: ${robotoMed.style.fontFamily};
+              --pt-sans: ${ptSans.style.fontFamily};
+              --karton: ${karton.style.fontFamily};
+              --karton2: ${karton2.style.fontFamily};
+            }
+          `}</style>
+        </head>
 
-      <body>
-        <Header />
-        <main className=' container'>{children}</main>
-        <Footer />
-      </body>
-    </html>
-          </ThemeProvider>
+        <body>
+          <Header />
+          <main className=' container'>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
