23f2712685583b816ed50250dabf4eeb34223c35
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-08-06 19:01:29 +0200

    FS-42 fix: fix metadata, transfer style to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,36 @@
 'use client';
 import '@/styles/index.scss';
-import type { Metadata } from 'next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { robotoReg, robotoMed, literalLight, literalBold, literalReg, ptSans, karton, karton2 } from '@/utils/fonts';
 
-export const metadata: Metadata = {
-  title: 'Flowers store',
-  description: `Discover the enchantment of nature's finest blooms. Fresh and artistic floral arrangements for every occasion. Unforgettable moments await at our Flowers Store.`,
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <style jsx global>{`
-        :root {
-          --literal-reg: ${literalReg.style.fontFamily};
-          --literal-bold: ${literalBold.style.fontFamily};
-          --literal-light: ${literalLight.style.fontFamily};
-          --roboto-reg: ${robotoReg.style.fontFamily};
-          --roboto-med: ${robotoMed.style.fontFamily};
-          --pt-sans: ${ptSans.style.fontFamily};
-          --karton: ${karton.style.fontFamily};
-          --karton2: ${karton2.style.fontFamily};
-        }
-      `}</style>
+      <head>
+        <title>Flowers store</title>
+        <meta
+          name='description'
+          content="Discover the enchantment of nature\' s finest blooms. Fresh and artistic
+                  floral arrangements for every occasion. Unforgettable moments await at our Flowers Store."
+        />
+        <style jsx global>{`
+          :root {
+            --literal-reg: ${literalReg.style.fontFamily};
+            --literal-bold: ${literalBold.style.fontFamily};
+            --literal-light: ${literalLight.style.fontFamily};
+            --roboto-reg: ${robotoReg.style.fontFamily};
+            --roboto-med: ${robotoMed.style.fontFamily};
+            --pt-sans: ${ptSans.style.fontFamily};
+            --karton: ${karton.style.fontFamily};
+            --karton2: ${karton2.style.fontFamily};
+          }
+        `}</style>
+      </head>
+
       <body>
         <Header />
-        <main className='container'>{children}</main>
+        <main className=' container'>{children}</main>
         <Footer />
       </body>
     </html>
