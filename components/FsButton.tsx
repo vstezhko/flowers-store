81d568d83f37c74a952de6c1e3bb1ffd39f559f4
viktoriastezko <viktoriastezhko@gmail.com>
2023-08-06 21:29:11 +0200

    fix: remove comments

--- components/FsButton.tsx
+++ components/FsButton.tsx
@@ -4,13 +4,10 @@ import { Button } from '@mui/material';
 import { FsButtonType } from '@/types/enums';
 
 interface FSButtonParams {
-  // id: number;
   label?: string;
   icon?: ReactElement;
   loading?: boolean;
   disabled?: boolean;
-  // color: string;
-  // endIcon?: string;
   startIcon?: ReactElement;
   onClick: MouseEventHandler<HTMLButtonElement>;
   className?: FsButtonType;
@@ -19,18 +16,14 @@ interface FSButtonParams {
 
 const FsButton: React.FC<FSButtonParams> = props => {
   const {
-    // id,
     icon = null,
     loading,
     label,
     disabled,
-    // color,
-    // endIcon = null,
     startIcon = null,
     onClick = e => e,
     className = FsButtonType.REGULAR,
     children,
-    // ...other
   } = props;
   //
   const content = (
@@ -60,47 +53,3 @@ const FsButton: React.FC<FSButtonParams> = props => {
 };
 
 export default FsButton;
-
-// export default function TDXButton(props) {
-//   const {
-//     id,
-//     bg,
-//     size,
-//     label,
-//     availability,
-//     loading,
-//     disabled,
-//     endIcon = null,
-//     startIcon = null,
-//     onClick = e => e,
-//     className = '',
-//     ...other
-//   } = props
-//
-//   const content = (
-//       <span className="tdx-button__content">
-//             {props.children}
-//         {availability ? <p className="tdx-button__availability">{availability}</p> : null}
-//         {label ? <span className="tdx-button__label">{label}</span> : null}
-//         </span>
-//   )
-//
-//   const handleClick = e => {
-//     !loading && onClick(e)
-//   }
-//
-//   return (
-//       <Button
-//           {...other}
-//           id={id}
-//           className={`tdx-button ${size} ${'bg-' + bg} ${loading ?? ''} ${className ?? ''}`}
-//           variant="contained"
-//           disabled={disabled}
-//           startIcon={loading ? null : startIcon}
-//           endIcon={loading ? null : endIcon}
-//           disableElevation
-//           onClick={handleClick}>
-//         {loading ? <CircularProgress color="inherit" size={16} /> : content}
-//       </Button>
-//   )
-// }
