828edc407931d44bc950ffb8fc5b4a7e6df5a8b4
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-08-06 18:49:34 +0200

    FS-72 feat: start creating footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MenuParams } from '@/components/Header';
 import NavMenu from '@/components/NavMenu';
+// import PhoneIcon from '@/components/Icons/PhoneIcon';
 
 export type MenuParamsWithoutPathName = Pick<MenuParams, 'id' | 'path' | 'title' | 'icon'>;
 
@@ -12,11 +13,38 @@ const menuItems: MenuParamsWithoutPathName[] = [
   { id: 6, path: '/offer', title: 'Offer', icon: null },
   { id: 5, path: '/info', title: 'Info for clients', icon: null },
 ];
+
+const getCurrentYear = () => {
+  return new Date().getFullYear();
+};
 const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer-nav'>
         <NavMenu menuItems={menuItems} currency={false} />
+      </div>
+      <div className='footer-menu'>
+        <div className='footer-menu__container container'>
+          <div className='footer-menu__item menu__info'>
+            <h3>Contact Information</h3>
+            {/*<p>Address</p>*/}
+            {/*<div className='menu__info-phone'>*/}
+            {/*  <PhoneIcon />*/}
+            {/*  <a className='phone' href='tel:+78083535335'>*/}
+            {/*    + 7 808 353 53 35*/}
+            {/*  </a>*/}
+            {/*</div>*/}
+            {/*<p>Shop hours: Mon-Sat from 8 am to 10 pm</p>*/}
+          </div>
+          <div className='footer-menu__item menu__clients'>
+            <h3>For Clients</h3>
+          </div>
+          <div className='footer-menu__item menu__form'>
+            <h3>Any questions? Contact us</h3>
+          </div>
+        </div>
+        <span className='line'></span>
+        <div className='footer__date'>© {getCurrentYear()} Flowers store</div>
       </div>
     </footer>
   );
