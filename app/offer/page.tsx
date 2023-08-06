31fccd646ad713a1d6f1c23da3f98139b8b2ae39
viktoriastezko <viktoriastezhko@gmail.com>
2023-08-06 23:04:07 +0200

    feat: add info to offer page

--- app/offer/page.tsx
+++ app/offer/page.tsx
@@ -1,7 +1,108 @@
 'use client';
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div>
+      <h1>OFFER AGREEMENT FOR FLOWER SHOP WITH DELIVERY</h1>
+
+      <p>
+        This Offer Agreement (hereinafter referred to as the &quot;Agreement&quot;) constitutes an official offer from
+        the Flower Shop with Delivery (hereinafter referred to as the &quot;Seller&quot; or &quot;Shop&quot;) to
+        conclude a purchase and sale agreement for flowers and flower delivery services (hereinafter referred to as the
+        &quot;Product&quot;) with any individual or legal entity (hereinafter referred to as the &quot;Buyer&quot; or
+        &quot;Client&quot;).
+      </p>
+
+      <h2>1. Definitions and Terms</h2>
+
+      <p>1.1. Seller: A legal entity engaged in the sale of flowers and the provision of delivery services.</p>
+
+      <p>
+        1.2. Buyer: Any individual or legal entity accepting this offer and concluding an agreement with the Seller on
+        the terms set forth in this Agreement.
+      </p>
+
+      <p>1.3. Product: Flowers, bouquets, floral arrangements, and other goods offered by the Seller.</p>
+
+      <p>
+        1.4. Delivery Services: Services related to the delivery of the ordered flowers and floral arrangements to the
+        address specified by the Buyer.
+      </p>
+
+      <h2>2. Subject of the Agreement</h2>
+
+      <p>
+        2.1. The Seller undertakes to provide the Buyer with the opportunity to purchase the Product and order Delivery
+        Services in accordance with the terms of this Agreement.
+      </p>
+
+      <p>
+        2.2. The Buyer agrees to purchase the Product and/or order Delivery Services in accordance with the terms of
+        this Agreement.
+      </p>
+
+      <h2>3. Order Placement and Payment</h2>
+
+      <p>
+        3.1. The Buyer can place an order on the Shop&apos;s website or via the contact phone number provided on the
+        website.
+      </p>
+
+      <p>3.2. The cost of the Product and Delivery Services is indicated on the Shop&apos;s website.</p>
+
+      <p>3.3. The Buyer shall make payment for the order according to the selected Products and Delivery Services.</p>
+
+      <h2>4. Delivery</h2>
+
+      <p>
+        4.1. The Seller undertakes to deliver the Product to the address specified by the Buyer within the specified
+        timeframe.
+      </p>
+
+      <p>
+        4.2. The Seller is not liable for delivery delays caused by force majeure circumstances (acts of God, natural
+        disasters, etc.).
+      </p>
+
+      <h2>5. Warranties</h2>
+
+      <p>5.1. The Seller guarantees the quality and freshness of all provided flowers and floral arrangements.</p>
+
+      <p>
+        5.2. In case the received Product does not match the description at the time of order placement, the Buyer has
+        the right to contact the Seller for a refund or replacement of the Product.
+      </p>
+
+      <h2>6. Confidentiality</h2>
+
+      <p>
+        6.1. The Seller undertakes to process the Buyer&apos;s personal data in accordance with the current legislation
+        on personal data protection.
+      </p>
+
+      <p>
+        6.2. The Seller does not disclose the Buyer&apos;s personal data to third parties without the Buyer&apos;s
+        consent.
+      </p>
+
+      <h2>7. Final Provisions</h2>
+
+      <p>
+        7.1. This Agreement comes into force from the moment the Buyer accepts the offer and remains in effect until
+        both parties fulfill their obligations.
+      </p>
+
+      <p>
+        7.2. The Seller reserves the right to make changes to the terms of this Agreement, which come into force from
+        the moment of publication on the Shop&apos;s website.
+      </p>
+
+      <p>
+        7.3. All disputes and disagreements arising from the performance of this Agreement are resolved through
+        negotiations between the parties.
+      </p>
+    </div>
+  );
 };
 
 export default Offer;
