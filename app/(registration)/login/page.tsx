3ba267bea51c729611c18848b6731e6cf612e085
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-08-07 23:14:26 +0200

    FS-47 feat: add image

--- app/(registration)/login/page.tsx
+++ app/(registration)/login/page.tsx
@@ -1,7 +1,12 @@
 'use client';
+import Leaf from '@/public/img/png/leaf.png';
 
 const Login = () => {
-    return <div>Login</div>;
+  return (
+    <div>
+      <img src={Leaf.src} alt='leaf' />
+    </div>
+  );
 };
 
 export default Login;
