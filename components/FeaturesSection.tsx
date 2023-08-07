505c0bc82786ccaeb9edd004dde63521f12f9763
Kseniya Biarezina <ksenia-2010@tut.by>
2023-08-07 23:02:14 +0300

    FS-77 refactor: change itemData structure

--- components/FeaturesSection.tsx
+++ components/FeaturesSection.tsx
@@ -6,58 +6,49 @@ import DeliveryHours from '@/public/img/png/delivery-hours.png';
 import Discounts from '@/public/img/png/discounts.png';
 
 export default function FeaturesSection() {
-  const itemData = [
+  const itemsData = [
     {
       img: Delivery.src,
-      text: [
-        <span key='free-delivery' className='features__highlight'>
-          Free delivery
-        </span>,
-        ' within the city',
-      ],
+      textStart: '',
+      highlight: 'Free delivery',
+      textEnd: ' within the city',
       alt: 'Delivery',
     },
     {
       img: Gifts.src,
-      text: [
-        'Postcard ',
-        <span key='as-a-gift' className='features__highlight'>
-          as a gift
-        </span>,
-        ' and a photo of delivery',
-      ],
+      textStart: 'Postcard ',
+      highlight: 'as a gift',
+      textEnd: ' and a photo of delivery',
       alt: 'Gifts',
     },
     {
       img: DeliveryHours.src,
-      text: [
-        <span key='round-the-clock' className='features__highlight'>
-          Round the clock
-        </span>,
-        ' delivery',
-      ],
+      textStart: '',
+      highlight: 'Round the clock',
+      textEnd: ' delivery',
       alt: '24/7',
     },
     {
       img: Discounts.src,
-      text: [
-        'Сumulative ',
-        <span key='discount-system' className='features__highlight'>
-          discount system
-        </span>,
-      ],
+      textStart: 'Cumulative ',
+      highlight: 'discount system',
+      textEnd: '',
       alt: 'Discounts',
     },
   ];
   return (
     <section className='features'>
       <ul className='features__galery'>
-        {itemData.map(item => (
+        {itemsData.map(item => (
           <li className='features__item' key={item.img}>
             <div className='features__img-container'>
               <Image fill={true} src={item.img} alt={item.alt} className='features__img' />
             </div>
-            <div className='features__text'> {item.text.map(fragment => fragment)}</div>
+            <div className='features__text'>
+              {item.textStart}
+              <span className='features__highlight'>{item.highlight}</span>
+              {item.textEnd}
+            </div>
           </li>
         ))}
       </ul>
