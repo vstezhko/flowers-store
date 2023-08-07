a89f2ad0ed668f9010e590eea4d1592779a38755
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-08-08 00:59:24 +0200

    FS-47 feat: add form

--- app/(registration)/login/page.tsx
+++ app/(registration)/login/page.tsx
@@ -1,10 +1,23 @@
 'use client';
+import LeafLeft from '@/public/img/png/leaf-left.png';
+import LeafRight from '@/public/img/png/leaf-right.png';
 import Leaf from '@/public/img/png/leaf.png';
+import { Paper } from '@mui/material';
 
 const Login = () => {
   return (
-    <div>
-      <img src={Leaf.src} alt='leaf' />
+    <div className='login__background-img'>
+      <div className='background-img background-img_left'>
+        <img src={LeafLeft.src} alt='leaf' />
+      </div>
+      <div className='background-img background-img_right'>
+        <img src={LeafRight.src} alt='leaf' />
+      </div>
+      <Paper elevation={3}>
+        <form className='login__form'>
+          <img src={Leaf.src} alt='leaf' className='form-img' />
+        </form>
+      </Paper>
     </div>
   );
 };
