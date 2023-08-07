8e2aab71ee3905226021f347c96068bfa231577f
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-08-07 18:47:36 +0200

    FS-72 feat: add test

---
+++ __tests__/footer.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import Footer from '@/components/Footer';
+
+describe('Footer', () => {
+  it('renders footer', () => {
+    render(<Footer />);
+    const footer = screen.getByRole('contentinfo');
+    expect(footer).toBeInTheDocument();
+  });
+});
