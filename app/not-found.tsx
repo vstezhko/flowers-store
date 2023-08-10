55b414dc7f2be2a3d893f447bc64da8bbd514133
Kseniya Biarezina <ksenia-2010@tut.by>
2023-08-10 13:31:32 +0300

    FS-65 fix: remove header&footer

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import NotFoundImg from '@/public/img/png/not-found.png';
-import MainLayout from '@/app/(main)/layout';
 import FsButton from '@/components/UI/FsButton';
+import { FsButtonType } from '@/types/enums';
 
 export default function NotFound() {
   const router = useRouter();
@@ -13,21 +13,20 @@ export default function NotFound() {
     router.push('/');
   };
   return (
-    <MainLayout>
-      <section className='error'>
-        <div className='error__container'>
-          <div className='error__img-container'>
-            <Image className='error__img' fill={true} src={NotFoundImg.src} alt='Not found' />
-          </div>
-          <div className='error__text-block'>
-            <h3>Oops... Something went wrong</h3>
-            <p>
-              It looks like the page you&apos;re looking for wasn&apos;t found. While we&apos;re fixing it, check page.
-            </p>
-          </div>
-          <FsButton onClick={handleBtnClick} label='Home' />
+    <section className='error'>
+      <div className='error__container'>
+        <div className='error__img-container'>
+          <Image className='error__img' fill={true} src={NotFoundImg.src} alt='Not found' />
         </div>
-      </section>
-    </MainLayout>
+        <div className='error__text-block'>
+          <h1>Oops... Something went wrong</h1>
+          <h3>
+            It looks like the page you&apos;re looking for wasn&apos;t found. While we&apos;re fixing it, check out our
+            Home page.
+          </h3>
+        </div>
+        <FsButton onClick={handleBtnClick} label='Home' className={FsButtonType.BIG} />
+      </div>
+    </section>
   );
 }
