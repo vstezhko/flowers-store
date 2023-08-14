707ca4f049b616fed03b3de1a2b139a22fb8fd46
viktoriastezko <viktoriastezhko@gmail.com>
2023-08-14 10:01:34 +0200

    FS-86 fix: try to fix error

--- components/UI/FsInput.tsx
+++ components/UI/FsInput.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState } from 'react';
 import { IconButton, InputAdornment, StandardTextFieldProps, TextField } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
