02b5022b2ecfa63a2444d9c6b3b53df4cb1896b4
Kseniya Biarezina <ksenia-2010@tut.by>
2023-08-24 15:14:37 +0200

    FS-104 feat: add link wrapper and button

--- components/catalog/SmallCard.tsx
+++ components/catalog/SmallCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import { Box, Paper } from '@mui/material';
+import { Box, Button, Paper } from '@mui/material';
+import Link from 'next/link';
 
 const SmallProductCard = ({
   productName,
@@ -14,16 +15,25 @@ const SmallProductCard = ({
   image: string;
 }) => {
   return (
-    <Paper className='small-card'>
-      <div className='small-card__image-container'>
-        <Image className='small-card__image' src={image} alt='Product photo' layout='fill'></Image>
-      </div>
-      <div className='small-card__name'>{productName}</div>
-      <Box component='div' sx={{ textOverflow: 'ellipsis' }} className='small-card__description'>
-        {description}
-      </Box>
-      <div className='small-card__price'>{price}</div>
-    </Paper>
+    <Link href='/catalog'>
+      <Paper className='small-card'>
+        <div className='small-card__image-container'>
+          <Image className='small-card__image' src={image} alt='Product photo' layout='fill'></Image>
+        </div>
+        <div className='small-card__text-content'>
+          <div className='small-card__name'>{productName}</div>
+          <Box component='div' sx={{ textOverflow: 'ellipsis' }} className='small-card__description'>
+            {description}
+          </Box>
+          <div className='small-card__details'>
+            <div className='small-card__price'>{price}</div>
+            <Button className='small-card__button' variant='outlined'>
+              More
+            </Button>
+          </div>
+        </div>
+      </Paper>
+    </Link>
   );
 };
 
