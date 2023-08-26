a52fa6514ed6c5ef3df68586503c62fb5ee0fdaf
Kseniya Biarezina <ksenia-2010@tut.by>
2023-08-26 09:37:31 +0200

    FS-109 feat: create searchBar component

---
+++ components/catalog/SearchBar.tsx
@@ -0,0 +1,38 @@
+import { SearchOutlined } from '@mui/icons-material';
+import { Divider, IconButton, InputBase, InputBaseProps, Paper } from '@mui/material';
+import { useState } from 'react';
+
+type SearchbarProps = {
+  onSubmit(searchTerm: string): void;
+  inputProps: InputBaseProps;
+  className?: string;
+};
+const Searchbar = (props: SearchbarProps) => {
+  const [searchItem, setSearchItem] = useState('');
+
+  return (
+    <Paper
+      className='searchbar'
+      component='form'
+      elevation={0}
+      onSubmit={e => {
+        e.preventDefault();
+        props.onSubmit((searchItem as string) ?? '');
+      }}>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder='Search...'
+        value={searchItem}
+        onChange={e => {
+          setSearchItem(e.target.value);
+        }}
+        {...props.inputProps}
+      />
+      <Divider className='searchbar__divider' orientation='vertical' />
+      <IconButton type='submit'>
+        <SearchOutlined />
+      </IconButton>
+    </Paper>
+  );
+};
+export default Searchbar;
