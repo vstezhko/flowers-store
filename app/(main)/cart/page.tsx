a83f296b334e442428b261bdfaaae656d6bc1665
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-09-07 17:52:02 +0200

    FS-148 feat: add link to cart icon, create page, add badge

---
+++ app/(main)/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <section className='page'>
+      <h1 className='page__title'>Cart</h1>
+    </section>
+  );
+};
+
+export default Cart;
--- components/header/HeaderCart.tsx
+++ components/header/HeaderCart.tsx
@@ -1,13 +1,18 @@
 import CartIcon from '@/components/Icons/CartIcon';
+import Badge from '@mui/material/Badge';
 
 export interface HeaderCartParams {
   sum: string;
+  invisible: boolean;
+  quantity: number;
 }
 
 const HeaderCart = (props: HeaderCartParams) => {
   return (
     <div className='header-menu__cart'>
-      <CartIcon />
+      <Badge badgeContent={props.quantity} color='primary' invisible={props.invisible}>
+        <CartIcon />
+      </Badge>
       <div className='cart__sum'>$ {props.sum}</div>
     </div>
   );
