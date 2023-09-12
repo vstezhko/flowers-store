f9a9606e376881a757d73df1ba3588fac7bf7a14
Kseniya Biarezina <ksenia-2010@tut.by>
2023-09-12 14:49:33 +0200

    FS-144 feat: add all team members info

--- components/about/TeamMemberCard.tsx
+++ components/about/TeamMemberCard.tsx
@@ -1,78 +1,71 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import Image from 'next/image';
 import Paper from '@mui/material/Paper';
 import noImage from '@/public/img/jpeg/no-image.jpg';
 import Slider from '@mui/material/Slider';
-import Photo from '@/public/img/jpeg/team-members/vika.jpg';
 import GithubIcon from '../Icons/social/GithubIcon';
 import LinkedinIcon from '../Icons/social/LinkedinIcon';
 import Link from 'next/link';
+import { TeamMemberParams } from '@/app/(main)/about/page';
 
 function valuetext(value: number) {
   return `${value}%`;
 }
 
-const TeamMemberCard = () => {
-  const [src, setSrc] = useState(Photo);
+function highlightText(text: string, highlights: string[]) {
+  const parts = text.split(new RegExp(`(\\b(?:${highlights.join('|')})\\b)`, 'i'));
+
+  return parts.map((part, index) => {
+    if (highlights.includes(part)) {
+      return (
+        <span key={index} className='highlight'>
+          {part}
+        </span>
+      );
+    }
+    return part;
+  });
+}
+
+const TeamMemberCard = ({ teamMemberData, isEven }: { teamMemberData: TeamMemberParams; isEven: boolean }) => {
+  const [src, setSrc] = useState(teamMemberData.photo);
+  const memoizedHighlightText = useMemo(
+    () => highlightText(teamMemberData.text, teamMemberData.highlights),
+    [teamMemberData.text, teamMemberData.highlights]
+  );
 
   return (
-    <Paper className='team-member' elevation={0}>
+    <Paper className={`team-member ${isEven ? 'even' : 'odd'}`}>
       <div className='team-member__biography'>
-        <h4 className='team-member__name'>Viktoria Stezhko</h4>
-        <h5 className='team-member__role highlight'>Team-Lead | Front-end Dev</h5>
-        <div className='team-member__text'>
-          I&apos;m a creative and adaptable professional with{' '}
-          <span className='highlight'>excellent communication skills</span>. After gaining valuable{' '}
-          <span className='highlight'>experience in management</span> and receiving positive reviews for{' '}
-          <span className='highlight'>streamlining business processes</span>, I moved into the IT industry, where
-          I&apos;ve been working for 1.5 years. My experience includes notable projects such as{' '}
-          <span className='highlight'>a website for an online film festival</span> and{' '}
-          <span className='highlight'>a web app for B2B sales platform</span>.
-        </div>
+        <h4 className='team-member__name'>{teamMemberData.name}</h4>
+        <h5 className='team-member__role highlight'>{teamMemberData.role}</h5>
+        <div className='team-member__text'>{memoizedHighlightText}</div>
       </div>
       <div className='team-member__image-block'>
         <Image className='team-member__image' src={src} onError={() => setSrc(noImage)} alt='Team member photo' />
         <div className='team-member__social'>
-          <Link href='https://github.com/vstezhko'>
+          <Link href={teamMemberData.githubLink}>
             <GithubIcon />
           </Link>
-          <Link href='https://www.linkedin.com/in/viktoria-stezhko'>
+          <Link href={teamMemberData.linkedinLink}>
             <LinkedinIcon />
           </Link>
         </div>
       </div>
       <div className='team-member__contributions'>
-        <div className='team-member__skill'>
-          <label>Communication</label>
-          <Slider
-            className='team-member__contributions-range'
-            aria-label='Communication'
-            value={95}
-            getAriaValueText={valuetext}
-            valueLabelDisplay='on'
-          />
-        </div>
-        <div className='team-member__skill'>
-          <label>Process Improvement</label>
-          <Slider
-            className='team-member__contributions-range'
-            aria-label='Process Improvement'
-            value={85}
-            getAriaValueText={valuetext}
-            valueLabelDisplay='on'
-          />
-        </div>
-        <div className='team-member__skill'>
-          <label>Project Management</label>
-          <Slider
-            className='team-member__contributions-range'
-            aria-label='Project Management'
-            value={100}
-            getAriaValueText={valuetext}
-            valueLabelDisplay='on'
-          />
-        </div>
+        {teamMemberData.skills.map((skill, index) => (
+          <div className='team-member__skill' key={index}>
+            <label>{skill[0]}</label>
+            <Slider
+              className='team-member__contributions-range'
+              aria-label={skill[0]}
+              value={skill[1]}
+              getAriaValueText={valuetext}
+              valueLabelDisplay='on'
+            />
+          </div>
+        ))}
       </div>
     </Paper>
   );
