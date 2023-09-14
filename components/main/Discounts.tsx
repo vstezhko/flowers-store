6e62fbdf991ffccc7e3c5330b4cfd8714bcf08d0
viktoriastezko <viktoriastezhko@gmail.com>
2023-09-14 13:49:11 +0200

    FS-141 feat: add banner on the main page

--- components/main/Discounts.tsx
+++ components/main/Discounts.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import DiscountsImg from '@/public/img/jpeg/discounts.jpg';
 import Image from 'next/image';
 import { FsButtonType } from '@/types/enums';
@@ -9,44 +9,58 @@ import { useSelector, useDispatch } from '@/redux/store';
 import { TokenService } from '@/api/services/Token.service';
 import { getCategoriesAsync } from '@/redux/slices/categorySlice/thunks';
 import { actions as searchActions } from '@/redux/slices/searchSlice/searchSlice';
+import { CategoryFullData } from '@/redux/slices/categorySlice/categorySlice';
 
 const Discounts = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const { categories } = useSelector(state => state.category);
 
-  useEffect(() => {
-    const fetchCategories = () => {
-      const token = TokenService.getAccessTokenFromLS()?.token;
-      if (token) dispatch(getCategoriesAsync(token));
-    };
+  const fetchCategories = async () => {
+    const token = TokenService.getAccessTokenFromLS()?.token;
+    if (token) return dispatch(getCategoriesAsync(token));
+  };
 
-    if (!categories) fetchCategories();
-  }, []);
-
-  const findTargetCategory = () => {
-    if (categories) {
-      return Object.values(categories).find(i => i.name?.en === 'Mix');
+  const findTargetCategory = (categoriesArr: CategoryFullData[]) => {
+    if (categoriesArr?.length) {
+      return categoriesArr.find(i => i.name?.en === 'Mix');
     }
   };
 
-  const handleBtnClick = () => {
-    const categoryId = findTargetCategory()?.id;
-    if (categoryId) {
-      dispatch(searchActions.setCategoryId(categoryId));
-      router.push('/catalog');
+  const handleBtnClick = async () => {
+    let categoryId;
+    if (!categories) {
+      const res = await fetchCategories();
+      if (res) {
+        categoryId = findTargetCategory(Object.values(res.payload.results))?.id;
+      }
+    } else {
+      categoryId = findTargetCategory(Object.values(categories))?.id;
     }
+    dispatch(searchActions.setCategoryId(categoryId));
+    router.push('/catalog');
   };
   return (
     <div className='discounts'>
-      <Image src={DiscountsImg} alt={'discounts'} className='discounts__img' />
-      <div className='discounts__info'>
+      <div className='discounts__container-img'>
+        <Image
+          className='discounts__img'
+          src={DiscountsImg}
+          alt='discounts'
+          style={{
+            minHeight: '100%',
+            width: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      </div>
+      <div className='discounts__container-message'>
         <h3>
           RS Student Exclusive: Get <span> 50% Off </span>
           on all Bouquets in &apos;MIX&apos; Category with the cart coupon
-          <span> &apos;RS&apos; </span> at Our Flower Shop!
+          <span> &apos;RS&apos; </span>!
         </h3>
-        <FsButton className={FsButtonType.BIG} label='find in Catalog' onClick={handleBtnClick} />
+        <FsButton className={FsButtonType.MEDIUM} label='find in Catalog' onClick={handleBtnClick} />
       </div>
     </div>
   );
