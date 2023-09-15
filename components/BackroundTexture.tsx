179fdc00fadcf085d51ffcf1b9412860cb02849b
viktoriastezko <viktoriastezhko@gmail.com>
2023-09-15 14:10:21 +0200

    FS-156 feat: refactor login images

--- components/BackroundTexture.tsx
+++ components/BackroundTexture.tsx
@@ -3,7 +3,7 @@ import Texture from '@/public/img/png/texture.webp';
 import Image from 'next/image';
 
 const BackgroundTexture = () => {
-  return <Image src={Texture} alt={'texture'} className='texture' loading='lazy' />;
+  return <Image src={Texture} alt={'texture'} className='texture' loading='lazy' priority={false} />;
 };
 
 export default BackgroundTexture;
