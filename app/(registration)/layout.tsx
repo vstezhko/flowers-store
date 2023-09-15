135391de2e93eda4010c83d11fee1f0f9014a87d
viktoriastezko <viktoriastezhko@gmail.com>
2023-09-15 13:25:56 +0200

    FS-156 feat: refactor login images

--- app/(registration)/layout.tsx
+++ app/(registration)/layout.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Texture from '@/public/img/png/texture.webp';
+import Image from 'next/image';
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
-  return <div className='auth-page'>{children}</div>;
+  return (
+    <div className='auth-page'>
+      <Image src={Texture} alt={'texture'} className='auth-page__background' />
+      {children}
+    </div>
+  );
 }
