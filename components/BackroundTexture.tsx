215e7d915d2b9b1e16c5f1103c76044c6b55033d
viktoriastezko <viktoriastezhko@gmail.com>
2023-09-15 16:55:34 +0200

    FS-156 feat: change texture priority

--- components/BackroundTexture.tsx
+++ components/BackroundTexture.tsx
@@ -3,7 +3,7 @@ import Texture from '@/public/img/png/texture.webp';
 import Image from 'next/image';
 
 const BackgroundTexture = () => {
-  return <Image src={Texture} alt={'texture'} className='texture' priority={true} quality={75} />;
+  return <Image src={Texture} alt={'texture'} className='texture' priority={false} quality={75} />;
 };
 
 export default BackgroundTexture;
