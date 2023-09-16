38770623657631be33d343e0c8982c2eaff8bdca
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-09-16 10:48:49 +0200

    FS-157 refactor: refactor layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       <Header />
       <main className='main'>
         {usePathname() === '/' ? (
-          <>{children}</>
+          children
         ) : (
           <div className='main__container'>
             <BackgroundTexture />
